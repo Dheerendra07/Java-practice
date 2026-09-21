8be6753fcbdcc1057875e4159396a2d42e004007
Dheerendra07 <dheerendraraj07@gmail.com>
2026-09-21 22:34:15 +0530

    Soting

---
+++ sorting/bubble_sort.java
@@ -0,0 +1,19 @@
+package sorting;
+import java.util.Arrays;
+public class bubble_sort {
+    public static void main(String[] args) {
+        int[] arr = { 10, 50, 30, 20, 40 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+       System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+ 
+}
