783f81c42d97391093dd5bc0de4280cd9beb3eb2
Dheerendra07 <dheerendraraj07@gmail.com>
2026-09-21 22:58:41 +0530

    selectin sort practice

---
+++ selection_sort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class selection_sort {
+    public static void main(String[] args){
+        int[] array = {10, 30, 40, 20, 60, 50};
+
+        for(int i = 0; i<array.length-1; i++){
+            for(int j = i+1; j<array.length; j++){
+                if(array[i] > array[j]){
+                  int temp = array[i];
+                  array[i] = array[j];
+                  array[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+}
